5396958e0192be74e84d8962912fd41a5f30a0d9
Omar <omarbs13@gmail.com>
2020-03-08 18:31:28 -0600

    Se modifica el endpage

---
+++ WebEncuesta/Content/js/Wizscript.js
@@ -0,0 +1,13 @@
+﻿//function stepnext(n) {
+//    console.log(n);
+//    if (n != 0) {
+//        //$(".stepwizard-row a").switchClass('btn-primary','btn-default');
+//        $(".stepwizard-row a").removeClass('btn-primary');
+//        $(".stepwizard-row a").addClass('btn-default');
+//        $('.stepwizard a[href="#step-' + n + '"]').tab('show');
+//        //$('.stepwizard-row a[href="#step-'+n+'"]').switchClass('btn-default','btn-primary');
+//        $('.stepwizard-row a[href="#step-' + n + '"]').removeClass('btn-default');
+//        $('.stepwizard-row a[href="#step-' + n + '"]').addClass('btn-primary');
+//    }
+//}
+//stepnext(1);
